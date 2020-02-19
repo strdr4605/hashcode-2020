b54246fff0625a65ec410957764c480ed99e7108
Dragoș Străinu <str.dr4605@gmail.com>
2020-02-19 22:33:27 +0200

    dummy algorithm

--- practice-round/index.js
+++ practice-round/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// Read data from file
+
 const fileName = process.argv[2];
 
 const file = fs.readFileSync(fileName, { encoding: "ascii" });
@@ -7,4 +9,35 @@ const file = fs.readFileSync(fileName, { encoding: "ascii" });
 const data = file.split("\n");
 data.pop();
 
-console.log(data);
+// Start algorithm
+
+const [maxSlices, typesAmount] = data
+  .shift()
+  .split(" ")
+  .map(e => +e);
+
+const types = data
+  .shift()
+  .split(" ")
+  .map(e => +e);
+
+let typesOrdered = 0;
+const orderedTypesIndexes = [];
+
+let i = typesAmount - 1;
+let currentSlices = maxSlices;
+
+while (i > -1) {
+  if (currentSlices - types[i] >= 0) {
+    orderedTypesIndexes.unshift(i);
+    currentSlices -= types[i];
+    typesOrdered++;
+  }
+
+  i--;
+}
+
+// To write to file: node index.js a_example.in > a.out
+
+console.log(typesOrdered);
+console.log(orderedTypesIndexes.join(" "));
